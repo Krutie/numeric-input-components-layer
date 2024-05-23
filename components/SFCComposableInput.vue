681bd7c6065cb956f3f7df1d62a376d56346eff3
krutie <krutie.patel@gmail.com>
2024-05-23 17:13:48 +1000

    fix css

--- components/SFCComposableInput.vue
+++ components/SFCComposableInput.vue
@@ -1,60 +1,26 @@
 <template>
-    <div class="example">
+    <div class="py-4 text-center flex items-center justify-center flex-col">
         <span class="block pb-2 font-mono">SFC with Composable</span>
-        <kbd class="block"> quantity: {{ quantity }} </kbd>
-        <div
-            class="
-                grid grid-rows-2 grid-flow-col
-                gap-0
-                p-2
-                text-teal text-center
-                w-max
-                my-0
-                mx-auto
-            "
-        >
+        <kbd class="block"> quantity: ref({{ quantity }}) </kbd>
+        <div class="grid grid-rows-2 grid-flow-col gap-0 p-2 text-center w-max my-0 mx-auto">
             <input
                 type="number"
-                class="
-                    row-span-2
-                    p-4
-                    w-14
-                    text-gray-700
-                    rounded-tl-lg rounded-bl-lg
-                "
+                class="border-2 border-r-0 row-span-2 p-4 w-14 rounded-tl-lg rounded-bl-lg"
                 v-model.lazy="quantity"
             />
             <button
                 type="button"
-                class="
-                    up
-                    col-span-2
-                    w-6
-                    bg-purple-500
-                    text-gray-50
-                    rounded-tr-lg
-                    opacity-80
-                    hover:opacity-100
-                "
+                class="border-2 px-0.5 border-b bg-purple-500 text-gray-50 rounded-tr-lg hover:opacity-80"
                 @click="increment"
             >
-                <ri-arrow-up-s-line class="opacity-50" />
+            <Icon icon="ri-arrow-up-s-line" :ssr="true" class="text-xl"/>
             </button>
             <button
                 type="button"
-                class="
-                    down
-                    col-span-2
-                    w-6
-                    bg-purple-500
-                    text-gray-50
-                    rounded-br-lg
-                    opacity-80
-                    hover:opacity-100
-                "
+                class="border-2 px-0.5 border-t bg-purple-500 text-gray-50 rounded-br-lg hover:opacity-80"
                 @click="decrement"
             >
-                <ri-arrow-down-s-line class="opacity-50" />
+            <Icon icon="ri-arrow-down-s-line" :ssr="true" class="text-xl"/>
             </button>
         </div>
         <div v-if="validation.error" class="text-red-400">
@@ -66,7 +32,11 @@
 <script>
 import { ref } from "vue";
 import useNumericInput from "../composables/NumericInput";
+import { Icon } from '@iconify/vue';
 export default {
+    components: {
+        Icon,
+    },
     setup() {
         const min = 0;
         const max = 25;
--- components/SFCInput.vue
+++ components/SFCInput.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="example">
-        <span class="block pb-2 font-mono">SFC components</span>
-        <kbd class="block"> quantity: {{ quantity }} </kbd>
-        <div class="grid grid-rows-2 grid-flow-col gap-0 p-2 text-teal text-center w-max my-0 mx-auto">
+    <div class="py-4 text-center flex items-center justify-center flex-col">
+        <span class="block pb-2 font-mono">SFC Component</span>
+        <kbd class="block"> quantity: ref({{ quantity }}) </kbd>
+        <div class="grid grid-rows-2 grid-flow-col gap-0 p-2 text-center w-max my-0 mx-auto">
             <input type="number"
-                   class="row-span-2 p-4 w-14 text-gray-700 rounded-tl-lg rounded-bl-lg"
+                   class="border-2 border-r-0 row-span-2 p-4 w-14 rounded-tl-lg rounded-bl-lg"
                    v-model="quantity" />
             <button type="button"
-                    class="up col-span-2 w-6 bg-orange-500 text-gray-50 rounded-tr-lg opacity-80 hover:opacity-100"
+                    class="border-2 px-0.5 border-b bg-orange-500 text-gray-50 rounded-tr-lg hover:opacity-80"
                     @click="increment">
-                <ri-arrow-up-s-line class="opacity-50" />
+                    <Icon icon="ri-arrow-up-s-line" :ssr="true" class="text-xl"/>
             </button>
             <button type="button"
-                    class="down col-span-2 w-6 bg-orange-500 text-gray-50 rounded-br-lg opacity-80 hover:opacity-100"
+                    class="border-2 px-0.5 border-t bg-orange-500 text-gray-50 rounded-br-lg hover:opacity-80"
                     @click="decrement">
-                <ri-arrow-down-s-line class="opacity-50" />
+                <Icon icon="ri-arrow-down-s-line" :ssr="true" class="text-xl"/>
             </button>
         </div>
     </div>
@@ -22,7 +22,12 @@
 
 <script>
 import { ref } from "vue";
+import { Icon } from '@iconify/vue';
+
 export default {
+    components: {
+        Icon,
+    },
     setup() {
         const quantity = ref(0);
         const increment = () => {
@@ -54,27 +59,6 @@ input[type="number"] {
     /* for iPad, iPhone Safari */
     border-top-right-radius: 0;
     border-bottom-right-radius: 0;
-}
-
-/* Example */
-.example {
-    @apply py-4 text-center;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-
-    span {
-        @apply text-sm text-gray-400;
-    }
-
-    .centered-box {
-        @apply p-1 w-max my-0 mx-auto border-2 rounded-xl border-opacity-50 border-gray-800 mt-2;
-
-        span {
-            @apply text-sm text-gray-500;
-        }
-    }
 }
 </style>
 <!-- 
